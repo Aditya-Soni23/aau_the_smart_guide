95f9abb4070d5f0671cd27314396624a2808b228
Aditya Soni <77341618+Aditya-Soni23@users.noreply.github.com>
2023-12-10 21:22:18 +0530

    Add files via upload

---
+++ cat.js
@@ -0,0 +1,64 @@
+function getcat(){
+    window.location = "catalogs.html";
+}
+function home(){
+    window.location = "index.html";
+}
+function con(){
+    window.location = "cont.html";
+}
+
+
+
+
+
+
+function buynoww(){
+    var t= document.getElementById("itemname");
+    var name=t.innerText;
+    document.getElementById("proname").value = "Product Name: "+name;
+    var s= document.getElementById("itemamount");
+    var amount=s.innerText;
+    document.getElementById("proamount").value = "Product Amount: "+amount;
+}
+
+function buynoww2(){
+    var t= document.getElementById("itemname2");
+    var name=t.innerText;
+    document.getElementById("proname").value = "Product Name: "+name;
+    var s= document.getElementById("itemamount2");
+    var amount=s.innerText;
+    document.getElementById("proamount").value = "Product Amount: "+amount;
+}
+function buynoww3(){
+    var t= document.getElementById("itemname3");
+    var name=t.innerText;
+    document.getElementById("proname").value = "Product Name: "+name;
+    var s= document.getElementById("itemamount3");
+    var amount=s.innerText;
+    document.getElementById("proamount").value = "Product Amount: "+amount;
+}
+function buynoww4(){
+    var t= document.getElementById("itemname4");
+    var name=t.innerText;
+    document.getElementById("proname").value = "Product Name: "+name;
+    var s= document.getElementById("itemamount4");
+    var amount=s.innerText;
+    document.getElementById("proamount").value = "Product Amount: "+amount;
+}
+function buynoww5(){
+    var t= document.getElementById("itemname5");
+    var name=t.innerText;
+    document.getElementById("proname").value = "Product Name: "+name;
+    var s= document.getElementById("itemamount5");
+    var amount=s.innerText;
+    document.getElementById("proamount").value = "Product Amount: "+amount;
+}
+function buynoww6(){
+    var t= document.getElementById("itemname6");
+    var name=t.innerText;
+    document.getElementById("proname").value = "Product Name: "+name;
+    var s= document.getElementById("itemamount6");
+    var amount=s.innerText;
+    document.getElementById("proamount").value = "Product Amount: "+amount;
+}
